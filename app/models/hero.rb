479cf44b0e75e1717835da07689c2d3ab8485702
felipeferrari40 <felipeferrari40@gmail.com>
2023-10-26 16:42:54 -0300

    commit

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,7 +1,7 @@
 class Hero < ApplicationRecord
   validates :name, :token, presence: true
 
-  scope :by_token, ->(token) { where(token: token) }
+  scope :by_token, ->(token) { where(token) }
   scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
   scope :sorted_by_name, -> { order(:created_at) }
 end
